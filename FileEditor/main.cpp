c9819bf9faf7efecc9fb79acae865262ef79d397
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-05-05 12:31:50 +0400

    Start Work

--- FileEditor/main.cpp
+++ FileEditor/main.cpp
@@ -6,35 +6,6 @@ int main()
 
     FileEditor object("data.txt");
     object.print();
-    std::cout << std::endl;
-
-    object.addLineBetween(6, 14, "Hello World");
-    object.print();
-    object.updateFile("newdata.txt");
-
-    object.addNewLineIndex(7, "\tstd::cout << \"Sime is so cute\" << std::endl;");
-    object.print();
-    std::cout << std::endl;
-
-
- //   std::vector<std::string> data = object.separateBySpaces(7);
- //   std::cout << 7 << '\t';
- //   for (ushint index = 0; index < data.size(); ++index)
- //   {
-	//std::cout << data[index] << " ";
- //   }
- //   std::cout << std::endl;
- //   for (ushint index = 0; index < data.size(); ++index)
- //   {
-	//std::cout << index << "\t" << data[index] << std::endl;
- //   }
-
-    object.readFromFile("newdata.txt");
-    object.print();
-    std::cout << std::endl;
-    
-
-
 
     std::cout << std::endl << "End work main.cpp" << std::endl;
 
