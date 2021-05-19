edce2589f2d9a8cbb2ab94fd4f0efc5f052392e7
Vahagn Gishyan <vahagn.gishyan.a@gmail.com>
2021-05-19 19:19:01 +0400

    release

--- FileTextEditor/FileTextEditor.h
+++ FileTextEditor/FileTextEditor.h
@@ -8,42 +8,42 @@ private:
 
 public:
     //initialization
-    FileTextEditor() : FileEditor()
+                             FileTextEditor() : FileEditor()
     {
 
     }
-    FileTextEditor(std::string filename) : FileEditor(filename)
+                             FileTextEditor(std::string filename) : FileEditor(filename)
     {
 	init();
     }
     
-    void init(int indexStart = -1, int indexEnd = -1);
-    void clear();
+    void                     init(int indexStart = -1, int indexEnd = -1);
+    void                     clear();
 
     //for FileEditor
-    void updata();
+    void                     updata();
 
     //for base work 
-    void print() const;
-    int  size() const;
-    bool empty() const;
+    void                     print() const;
+    int                      size() const;
+    bool                     empty() const;
 
     //set, get, add and delete lines
     std::vector<std::string> getLine(int indexLine) const; 
-    void setLine(int indexLine, const std::vector<std::string>& newLine);
+    void                     setLine(int indexLine, const std::vector<std::string>& newLine);
 
-    void addNewLine(int indexLine, const std::vector<std::string>& newLine);
-    void deleteLine(int indexLine);
+    void                     addNewLine(int indexLine, const std::vector<std::string>& newLine);
+    void                     deleteLine(int indexLine);
 
-    void addNewLineBack(const std::vector<std::string>&  newLine);
-    void deleteLineBack();
+    void                     addNewLineBack(const std::vector<std::string>&  newLine);
+    void                     deleteLineBack();
 
     //work in lines
-    int  searchWordInLine(int indexLine, const std::string&) const;
-    int  getLineSize(int indexLine) const;
+    int                      searchWordInLine(int indexLine, const std::string&) const;
+    int                      getLineSize(int indexLine) const;
 
     //work form File
-    void readFormFile(std::string filename = "");
-    void updateFile();
+    void                     readFormFile(std::string filename = "");
+    void                     updateFile();
 };
 
